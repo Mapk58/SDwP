69e40951781ee9499955424c62d108a533b0b563
cmonkl <cmon86559@gmail.com>
2022-10-18 01:01:34 +0300

    docs

--- assigment_2/task_2/Model.py
+++ assigment_2/task_2/Model.py
@@ -3,21 +3,42 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
+
 class LogRegression:
+    """
+    Logistic Regression model
+    """
+
     def __init__(self):
-        self.model =  Pipeline([
-            ('preprocess', StandardScaler()),
-            ('model', LogisticRegression())
-        ])
+        self.model = Pipeline(
+            [("preprocess", StandardScaler()), ("model", LogisticRegression())]
+        )
 
     def load(self, load_path):
-        with open(load_path, 'rb') as f:
+        """
+        Load model weights
+
+        :param load_path: path to weights object
+        :return:
+        """
+        with open(load_path, "rb") as f:
             self.model = pickle.load(f)
 
     def save(self, save_path):
-        with open(save_path, 'wb') as f:
+        """
+        Save model weights
+
+        :param save_path: path to save model weights
+        :return:
+        """
+        with open(save_path, "wb") as f:
             pickle.dump(f, self.model)
 
     def predict(self, X):
-        return self.model.predict(X)
+        """
+        Get prediction from model
 
+        :param X: features
+        :return: predictions
+        """
+        return self.model.predict(X)
